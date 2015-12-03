f515c176eb4562b7b7fd4c6283f938b0c71cbc8d
sanguineswan <786piyushsinha@gmail.com>
2015-12-03 17:20:52 +0530

    Simple input change

--- InMemoryCache/src/com/hike/cache/test/ExpiryCacheActionTest.java
+++ InMemoryCache/src/com/hike/cache/test/ExpiryCacheActionTest.java
@@ -7,21 +7,21 @@ import com.hike.cache.action.ExpiryCacheAction;
 public class ExpiryCacheActionTest {
 	public static void main(String[] args) throws InterruptedException{
 		ExpiryCacheAction<String, String> cache = new ExpiryCacheAction<>();
-		cache.put("piyush", "sinha", 10, TimeUnit.SECONDS);
-		cache.put("bhavya", "swaroop", 20, TimeUnit.SECONDS);
+		cache.put("Flipkart", "Sachin Bansal", 10 , TimeUnit.SECONDS);
+        cache.put("Amazon", "Bezos", 20 , TimeUnit.SECONDS);
 		
-		System.out.println(cache.get("piyush"));
-		System.out.println(cache.get("bhavya"));
-		
-		Thread.sleep(TimeUnit.SECONDS.toMillis(10));
-		
-		System.out.println(cache.get("piyush"));
-		System.out.println(cache.get("bhavya"));
+		System.out.println(cache.get("Flipkart"));
+		System.out.println(cache.get("Amazon"));
 		
 		Thread.sleep(TimeUnit.SECONDS.toMillis(10));
 		
-		System.out.println(cache.get("piyush"));
-		System.out.println(cache.get("bhavya"));
+		System.out.println(cache.get("Flipkart"));
+		System.out.println(cache.get("Amazon"));
+		
+		Thread.sleep(TimeUnit.SECONDS.toMillis(10));
+		
+		System.out.println(cache.get("Flipkart"));
+		System.out.println(cache.get("Amazon"));
 		
 		
 	}
